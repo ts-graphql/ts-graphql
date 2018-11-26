55964e2b41faab30dfbec41327a7752adf11c570
stephentuso <tusodev@gmail.com>
2018-11-25 23:19:04 -0500

    Add type tests, circle ci config

--- src/decorators/__tests__/types.test.ts
+++ src/decorators/__tests__/types.test.ts
@@ -1,14 +1,39 @@
 import 'jest';
 import { exec } from 'child_process';
 import { readdir } from 'fs';
+import pify from 'pify';
+import path from 'path';
+
+// Would be better to do this programmatically, didn't look straightforward though.
+const typeCheckFiles = async (files: string[], expectError?: boolean) => {
+  await Promise.all(files.map(file => new Promise((resolve, reject) => {
+    const tscArgs = '--strict --experimentalDecorators --emitDecoratorMetadata  --noEmit --lib esnext';
+    exec(`$(npm bin)/tsc ${tscArgs} ${file}`, (err) => {
+      if ((err && expectError) || (!err && !expectError)) {
+        resolve();
+      } else {
+        reject(new Error(expectError
+          ? 'Types valid, expected invalid'
+          : 'Types invalid, expected valid',
+        ));
+      }
+    });
+  })));
+};
+
+const getFiles = async (directory: string): Promise<string[]> => {
+  const files = await pify(readdir)(path.join(__dirname, directory));
+  return files.map((file: string) => path.join(__dirname, directory, file));
+}
 
 describe('Decorator type validation', () => {
-  // TODO run tsc on invalid/valid files
-  it('passes for valid files', () => {
+  it('passes for valid files', async () => {
+    const validFiles = await getFiles('valid');
+    await typeCheckFiles(validFiles);
+  }, 30000);
 
-  });
-
-  it('fails for invalid files', () => {
-
-  });
+  it('fails for invalid files', async () => {
+    const invalidFiles = await getFiles('invalid');
+    await typeCheckFiles(invalidFiles, true);
+  }, 30000);
 });
