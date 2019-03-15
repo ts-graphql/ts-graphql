02dead73c20e9f96416ef6265eef1d4e7b7e2a0a
stephentuso <tusodev@gmail.com>
2019-03-15 00:59:20 -0400

    test: add test for incorrect subscription queries

--- src/__tests__/fields.test.ts
+++ src/__tests__/fields.test.ts
@@ -1,8 +1,37 @@
+import { SubscriptionFieldConfigMap } from '../fields';
+
 if (Symbol["asyncIterator"] === undefined) ((Symbol as any)["asyncIterator"]) = Symbol.for("asyncIterator");
 
 import 'jest';
 import { buildFields, fields, buildSubscriptionFields, subscriptionFields, TSGraphQLString } from '../index';
-import { GraphQLObjectType, GraphQLSchema, subscribe, parse } from 'graphql';
+import { GraphQLObjectType, GraphQLSchema, subscribe, parse, graphql } from 'graphql';
+
+const schemaForSubscriptionFields = (subFields: SubscriptionFieldConfigMap<any, any>) => {
+  const query = new GraphQLObjectType({
+    name: 'Query',
+    fields: buildFields(fields({}, (field) => ({
+      foo: field(
+        { type: TSGraphQLString },
+        () => 'Required query field',
+      ),
+    })))
+  })
+
+  const subscription = new GraphQLObjectType({
+    name: 'Subscription',
+    fields: buildSubscriptionFields(subFields),
+  });
+
+  return new GraphQLSchema({ query, subscription });
+}
+
+const querySubscriptionFields = (subFields: SubscriptionFieldConfigMap<any, any>, queryString: string) => {
+  const schema = schemaForSubscriptionFields(subFields);
+
+  const document = parse(queryString);
+
+  return subscribe(schema, document);
+}
 
 describe('fields', () => {
   describe('buildSubscriptionFields', () => {
@@ -17,30 +46,11 @@ describe('fields', () => {
         ),
       }));
 
-      const query = new GraphQLObjectType({
-        name: 'Query',
-        fields: buildFields(fields({}, (field) => ({
-          foo: field(
-            { type: TSGraphQLString },
-            () => 'Required query field',
-          ),
-        })))
-      })
-
-      const subscription = new GraphQLObjectType({
-        name: 'Subscription',
-        fields: buildSubscriptionFields(subFields),
-      });
-
-      const schema = new GraphQLSchema({ query, subscription });
-
-      const document = parse(`
+      const result = await querySubscriptionFields(subFields, `
         subscription {
           test
         }
       `);
-
-      const result = await subscribe(schema, document);
 
       const values = [];
       for await (const value of result as any) {
@@ -48,6 +58,28 @@ describe('fields', () => {
       }
 
       expect(values).toEqual(['A', 'B']);
+    });
+
+    it('should return error if subscription queried incorrectly', async () => {
+      const subFields = subscriptionFields({}, (field) => ({
+        test: field(
+          { type: TSGraphQLString },
+          async function* () {
+            yield 'A';
+            yield 'B';
+          },
+        ),
+      }));
+
+      const schema = schemaForSubscriptionFields(subFields);
+
+      const result = await graphql(schema, `
+        subscription {
+          test
+        }
+      `);
+
+      expect(result.errors).toBeTruthy();
     });
   });
 });
