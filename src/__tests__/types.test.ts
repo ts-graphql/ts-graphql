a1cccc5cad49b53f7675325acb5aa80b2ddd7b53
stephentuso <tusodev@gmail.com>
2019-05-23 01:29:34 -0400

    test: increase types test timeout

--- src/__tests__/types.test.ts
+++ src/__tests__/types.test.ts
@@ -5,10 +5,10 @@ describe('Root files type validation', () => {
   it('passes for valid files', async () => {
     const validFiles = await getFilesInDir(__dirname, 'valid');
     await typeCheckFiles(validFiles);
-  }, 30000);
+  }, 60000);
 
   it('fails for invalid files', async () => {
     const invalidFiles = await getFilesInDir(__dirname, 'invalid');
     await typeCheckFiles(invalidFiles, true);
-  }, 30000);
+  }, 60000);
 });
